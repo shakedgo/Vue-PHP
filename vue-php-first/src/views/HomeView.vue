525a4831370a3252f489b6799e502871f3262df5
student <shakedgo@gmail.com>
2023-01-08 23:24:35 +0200

    Added vue form with php backend" /

--- vue-php-first/src/views/HomeView.vue
+++ vue-php-first/src/views/HomeView.vue
@@ -1,3 +1,53 @@
 <template>
-	<h1>Hola</h1>
+	<form @submit.prevent="handleSubmit">
+		<!-- prevents from page refresh -->
+		<label>Name: </label>
+		<input type="text" required v-model="name" />
+		<br />
+		<label>Email: </label>
+		<input type="email" required v-model="email" />
+		<br />
+		<label>Password: </label>
+		<input type="password" required v-model="password" />
+		<div v-if="passError">{{ passError }}</div>
+		<br />
+		<button type="submit">Submit</button>
+	</form>
+
+	<p>Name: {{ name }}</p>
+	<p>Email: {{ email }}</p>
+	<p>Password: {{ password }}</p>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+	data() {
+		return {
+			name: "",
+			email: "",
+			password: "",
+			passError: "",
+		};
+	},
+	methods: {
+		handleSubmit() {
+			axios
+				.post(
+					"http://localhost:4000/",
+					JSON.stringify({
+						name: this.name,
+						email: this.email,
+						pass: this.password,
+					})
+				)
+				.then((res) => {
+					// Process the response data
+					console.log(res.data);
+				});
+		},
+	},
+};
+</script>
+
+<style></style>
